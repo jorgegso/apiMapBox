5051687aa6f2d37ac5c5710d659f3a9097666dff
mikiimikii <hajorge2012@gmail.com>
2020-08-23 05:29:26 -0300

    Updates

---
+++ precache-manifest.aa1cd4a1e7ed5d7d5256f6934b313d9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eaf9cc6276aef2fa6e1e569e46ac65d",
+    "url": "/jorgegso/mapBoxApi/index.html"
+  },
+  {
+    "revision": "750524dc3f7c368b3977",
+    "url": "/jorgegso/mapBoxApi/static/css/main.48c09ce7.chunk.css"
+  },
+  {
+    "revision": "10fcb5f8a199d72c3b79",
+    "url": "/jorgegso/mapBoxApi/static/js/2.766a5735.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jorgegso/mapBoxApi/static/js/2.766a5735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750524dc3f7c368b3977",
+    "url": "/jorgegso/mapBoxApi/static/js/main.d477245c.chunk.js"
+  },
+  {
+    "revision": "5f38c4f602372ebf2b82",
+    "url": "/jorgegso/mapBoxApi/static/js/runtime-main.9d0ff618.js"
+  }
+]);
